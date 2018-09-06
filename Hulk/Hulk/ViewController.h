d42795424f26c10124d36a863619c165ff62c633
Eyshen <zxc510789295@163.com>
2018-09-06 17:49:00 +0800

    提交

--- Hulk/Hulk/ViewController.h
+++ Hulk/Hulk/ViewController.h
@@ -8,7 +8,20 @@
 
 #import <UIKit/UIKit.h>
 
+@protocol NSCopying
+
+- (id)copyWithZone:(nullable NSZone *)zone;
+
+@end
+
+@protocol NSMutableCopying
+
+- (id)mutableCopyWithZone:(nullable NSZone *)zone;
+
+@end
+
 @interface ViewController : UIViewController
+
 
 
 @end
--- Hulk/Hulk/ViewController.m
+++ Hulk/Hulk/ViewController.m
@@ -8,21 +8,48 @@
 
 #import "ViewController.h"
 
+
+#define ZY_SCREEN_WIDTH             [UIScreen width]
+
+#define ZY_SCREEN_HEIGHT            [UIScreen height]
+
 @interface ViewController ()
+
+
 
 @end
 
 @implementation ViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIImageView *image = [[UIImageView alloc]initWithFrame:CGRectMake(100, 200, 15, 15)];
+    [image setImage:[UIImage imageNamed:@"purchase_hand"]];
+    [self.view addSubview:image];
+    
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation animation];
+    animation.keyPath = @"transform.translation.y";
+    animation.values = @[@0, @-20, @0, @20, @0];
+    animation.keyTimes = @[ @0, @(1 / 4), @(1 / 2), @(3 / 4), @1 ];
+    animation.duration = 0.8;
+    animation.repeatCount = MAXFLOAT;
+    animation.additive = YES;
+    [image.layer addAnimation:animation forKey:@"keyTimes"];
 }
 
+-(void)printN:(int)num{
+    if (num) {
+        [self printN:(num-1)];
+//        NSLog(@"%d\n",num);
+    }
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 
